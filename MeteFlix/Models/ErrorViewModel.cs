6a8ae367d3767d17515ef88e9334bb670963b2ee
presida07 <duduchimenas@gmail.com>
2023-04-18 11:41:30 -0300

    Criado o projeto

---
+++ MeteFlix/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MeteFlix.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ MeteFlix/Models/Genre.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MeteFlix.Models;
+
+    [Table("Genre")]
+    public class Genre
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public byte Id { get; set; }
+        
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O Nome é Obrigatório")]
+        [StringLength(30, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
+        public string Name { get; set; } 
+    }
+ 
