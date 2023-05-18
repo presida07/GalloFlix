d5adf70254f3703ecd0e5c9fbc832d09112045f3
presida07 <duduchimenas@gmail.com>
2023-05-18 11:30:58 -0300

    Criado layout de login

---
+++ GalloFlix/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+using GalloFlix.DataTransferObjects;
+using GalloFlix.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GalloFlix.Controllers;
+
+[Authorize(Roles = "Administrador")]
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(ILogger<AccountController> logger,
+        SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            LoginDto login = new();
+            login.ReturnUrl = returnUrl ?? Url.Content("~/");
+            return View(login);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
