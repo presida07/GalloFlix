5b0c3b7067541c477d2c82e17598b8b6f5010fb6
presida07 <duduchimenas@gmail.com>
2023-08-17 12:28:28 -0300

    TERMINOU PORRA

--- GalloFlix/wwwroot/js/site.js
+++ GalloFlix/wwwroot/js/site.js
@@ -26,3 +26,13 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 });
+
+(() => {
+    'use strict';
+    if (document.querySelector('#sidebarToggler') != null) {
+        document.querySelector('#sidebarToggler').addEventListener('click', () => {
+            document.querySelector('#sidebar').classList.toggle('d-none')
+        })
+    }
+})()
+
